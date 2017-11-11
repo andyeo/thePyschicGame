431585a569ae947cf2f9a2f61c130e20b025b178
Your Name <andy.ontaneda@gmail.com>
2017-11-11 00:34:30 -0500

    final i tried

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,40 @@
 $( document ).ready(function() {
     /*numbers for the score */
+    var numberGuesses =10;
     var numberWin =0;
     var numberLosses =0;
-    var numberGuesses =10;
-    var guessesMade;
-      
+        
+        var computerChoice = genCharArray('a', 'z')[Math.floor(Math.random()*genCharArray('a', 'z').length)];
+        console.log(computerChoice);
     
+        document.onkeyup = function (event) {
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); 
+        console.log(userGuess)
+        for (i=0; i < arrayGuesses; i++) {
+        var arrayGuesses = [" "];
+        arrayGuesses.push(userGuess);
+        console.log(arrayGuesses[i]);
+       
+       if (userGuess == computerChoice) {     
+           numberWin++;
+       }
+        else if (userGuess != computerChoice) {
+           numberLosses++;
+      console.log(userGuess);
+       }
+    }
+   
+   // var arrayGuesses= guessesMade;
     var instruction = "<p>guess what letter I am thinking of</p>"; 
     var displayWins = "<p>Wins: " + numberWin + "</p>"; 
     var displayLosses = "<p>Loses: " + numberLosses + "</p>"; 
     var displayGuessesLeft = "<p>Guesses Left: " + numberGuesses + "</p>";
-    var displayGuessesMade = "<p>Your Guesses so far: " + guessesMade + "</p>";
-    /* user choice */
-    for (i=0; i<10;i++){
-
-    }
-    /* possible choices for computer */    
-    var computerChoice = genCharArray('a', 'z')[Math.floor(Math.random()*genCharArray('a', 'z').length)];
-    
-    console.log(computerChoice);
+    var displayGuessesMade = "<p>Your Guesses so far: " + arrayGuesses + "</p>";
 
     document.querySelector('#gameScore').innerHTML = instruction + displayWins + displayLosses + 
-    displayGuessesLeft + displayGuessesMade 
+    displayGuessesLeft+ displayGuessesMade 
+            }          
+        }
     });
     
 
@@ -32,4 +45,4 @@ function genCharArray(charA, charZ) {
     }
     return a;
 }
-        
+       
