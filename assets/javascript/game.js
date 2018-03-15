b0e58b99882a03a14eb462f80e954fd685488a3b
andyeo <yourGitHub@email.com>
2018-03-15 17:05:35 -0400

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,71 @@
-$( document ).ready(function() {
-    /*numbers for the score */
-    var numberGuesses =10;
-    var numberWin =0;
-    var numberLosses =0;
-        
-        var computerChoice = genCharArray('a', 'z')[Math.floor(Math.random()*genCharArray('a', 'z').length)];
-        console.log(computerChoice);
-    
-        document.onkeyup = function (event) {
-        var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); 
-        console.log(userGuess)
-        for (i=0; i < arrayGuesses; i++) {
-        var arrayGuesses = [" "];
-        arrayGuesses.push(userGuess);
-        console.log(arrayGuesses[i]);
-       
-       if (userGuess == computerChoice) {     
-           numberWin++;
-       }
-        else if (userGuess != computerChoice) {
-           numberLosses++;
-      console.log(userGuess);
-       }
-    }
+
+  
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+   //Setting all to zero
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+
+
+//Lets the computer select a random letter from the available choices
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+//Allows the user 9 guesses
+// guesses = guesses || 9
+var updateGuessesLeft = function() {
+  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+  document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+
+var updateLetterToGuess = function() {
+  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+var updateGuessesSoFar = function() {
+  // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
+// Function will be called when we reset everything
+var reset = function() {
+  totalGuesses = 9;
+  guessesLeft = 9;
+  guessedLetters = [];
    
-   // var arrayGuesses= guessesMade;
-    var instruction = "<p>guess what letter I am thinking of</p>"; 
-    var displayWins = "<p>Wins: " + numberWin + "</p>"; 
-    var displayLosses = "<p>Loses: " + numberLosses + "</p>"; 
-    var displayGuessesLeft = "<p>Guesses Left: " + numberGuesses + "</p>";
-    var displayGuessesMade = "<p>Your Guesses so far: " + arrayGuesses + "</p>";
-
-    document.querySelector('#gameScore').innerHTML = instruction + displayWins + displayLosses + 
-    displayGuessesLeft+ displayGuessesMade 
-            }          
-        }
-    });
-    
-
-function genCharArray(charA, charZ) {
-    var a = [], i = charA.charCodeAt(0), j = charZ.charCodeAt(0);
-    for (; i <= j; ++i) {
-        a.push(String.fromCharCode(i));
-    }
-    return a;
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
 }
-       
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+
+//When key is released it becomes the users guess
+document.onkeyup = function(event) {
+   guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  guessedLetters.push(userGuess);
+  updateGuessesLeft();
+  updateGuessesSoFar();
+
+        if (guessesLeft > 0){
+            if (userGuess == letterToGuess){
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                alert("Yes, you are psychic!");
+                reset();
+            }
+        }else if(guessesLeft == 0){
+            // Then we will loss and we'll update the html to display the loss 
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            document.getElementById("loser").innerHTML = ("Sorry, you're not psychic, maybe try again?");
+            // Then we'll call the reset. 
+            reset();
+        }
+
+};
